28217d871dad3a278ca7af49423dc327170776c5
Neo-Ryo <marco.sch4064@gmail.com>
2021-05-10 12:08:05 +0200

    DATABASE_URL env

--- sequelize.js
+++ sequelize.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  dialect: 'postgres',
-  host: 'localhost',
-  logging: false,
-});
-
-module.exports = sequelize;
+if (process.env.DATABASE_URL) {
+  module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+  });
+} else {
+  module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    dialect: 'postgres',
+    host: 'localhost',
+    logging: false,
+  });
+}
