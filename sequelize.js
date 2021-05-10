07bf2d59e3652e14bcc67eb5c4142e5d33cff280
Neo-Ryo <marco.sch4064@gmail.com>
2021-05-10 12:22:15 +0200

    postgres ssl true

--- sequelize.js
+++ sequelize.js
@@ -5,6 +5,9 @@ const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 if (process.env.DATABASE_URL) {
   module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   });
 } else {
   module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
