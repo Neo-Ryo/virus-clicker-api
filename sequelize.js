c6dba4de27f38a655ac9035503a5e5cc83ba8e44
Neo-Ryo <marco.sch4064@gmail.com>
2021-05-11 10:08:03 +0200

    added heroku credentials

--- sequelize.js
+++ sequelize.js
@@ -4,7 +4,14 @@ const { Sequelize } = require('sequelize');
 const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 if (process.env.DATABASE_URL) {
-  module.exports = new Sequelize(process.env.DATABASE_URL);
+  module.exports = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    password: process.env.PASSWORD,
+    username: process.env.USER,
+    database: process.env.DATABASE_URL,
+  });
 } else {
   module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     dialect: 'postgres',
