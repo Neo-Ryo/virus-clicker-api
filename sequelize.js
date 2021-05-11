b4e45967f0f34f20aa8ec18c0e4c11cc0ffbfa28
Neo-Ryo <marco.sch4064@gmail.com>
2021-05-11 10:19:57 +0200

    environement ?

--- sequelize.js
+++ sequelize.js
@@ -6,11 +6,12 @@ const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 if (process.env.NODE_ENV === 'production') {
   module.exports = new Sequelize({
     dialect: 'postgres',
-    host: process.env.HOST,
-    port: process.env.PORT,
-    password: process.env.PASSWORD,
-    username: process.env.USER,
-    database: process.env.DATABASE_URL,
+    // host: process.env.HOST,
+    // port: process.env.PORT,
+    // password: process.env.PASSWORD,
+    // username: process.env.USER,
+    // database: process.env.DATABASE_URL,
+    ssl: true,
   });
 } else {
   module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
