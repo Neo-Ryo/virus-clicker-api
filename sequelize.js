01e85bc78a38936397fddc82d4f1e5838edccb57
Neo-Ryo <marco.sch4064@gmail.com>
2021-05-11 09:50:43 +0200

    testing

--- sequelize.js
+++ sequelize.js
@@ -3,13 +3,8 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
-if (process.env.PRODUCTION) {
-  module.exports = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    logging: false,
-    ssl: true,
-  });
+if (process.env.DATABASE_URL) {
+  module.exports = new Sequelize(process.env.DATABASE_URL);
 } else {
   module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     dialect: 'postgres',
